d60a6a190d0ec4361e63d0aac0233bbe5a1db559
fsamo <fsamonte99@gmail.com>
2022-04-25 09:02:58 -0400

    Version 1.14.2 - added Amazon URL and removed ASIN column in Results table. Added background color and centered search bar in Home Page.

--- Amazon_Scraper/Amazon_Scraper/spiders/amazon.py
+++ Amazon_Scraper/Amazon_Scraper/spiders/amazon.py
@@ -57,5 +57,7 @@ class AmazonSpider(scrapy.Spider):
             sizes = di.get('size_name', [])
             colors = di.get('color_name', [])
         bullet_points = response.xpath('//*[@id="feature-bullets"]//li/span/text()').extract()
-        yield {'asin': asin, 'Title': title, 'MainImage': image, 'Rating': rating, 'NumberOfReviews': number_of_reviews,
-               'Price': price, 'AvailableSizes': sizes, 'AvailableColors': colors, 'BulletPoints': bullet_points}
+        url = 'https://www.amazon.com/dp/' + asin
+        yield {'Title': title, 'MainImage': image, 'Rating': rating, 'NumberOfReviews': number_of_reviews,
+               'Price': price, 'AvailableSizes': sizes, 'AvailableColors': colors, 'BulletPoints': bullet_points,
+               'URL': url}
